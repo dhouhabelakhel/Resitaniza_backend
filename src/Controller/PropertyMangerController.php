170cddb3d59e0ac4ba34da58cb6b81daed771fd6
dhouha belakhel <dhouhabelakhel2001@gmail.com>
2024-05-09 21:50:54 +0200

    add photo upload to manager

--- src/Controller/PropertyMangerController.php
+++ src/Controller/PropertyMangerController.php
@@ -9,12 +9,15 @@ use App\Repository\PropertyMangerRepository;
 use App\Entity\PropertyManger;
 use App\Entity\Residence;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/Api/PropertyManger')]
 
@@ -26,32 +29,61 @@ class PropertyMangerController extends AbstractController
         return $this->json($propertyManger->findAll(), JsonResponse::HTTP_OK);
     }
     #[Route('/new', name: 'app_manger_new', methods: ['POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, UserPasswordHasherInterface $hasher)
-    {
-        $manger = $serializer->deserialize($request->getContent(), PropertyManger::class, 'json');
+    public function new(ValidatorInterface $validator,PropertyMangerRepository $manger, Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, UserPasswordHasherInterface $hasher)
+    {   $newmanger = $serializer->deserialize($request->getContent(), PropertyManger::class, 'json');
+        $existedemail = $manger->findOneBy(['email' => $newmanger->getEmail()]);
+        $existedcin = $manger->findOneBy(['cin' => $newmanger->getCin()]);
+        if ($existedemail || $existedcin) {
+            return $this->json("alerdy exist", JsonResponse::HTTP_FORBIDDEN);
+        } else {
+            $photo=$newmanger->getPicture();
+            if($photo instanceof UploadedFile){
+                $fileTypeValidator = new Assert\File([
+                    'maxSize' => '2M', 
+                    'mimeTypes' => [
+                        'image/jpeg',
+                        'image/png',
+                        'image/gif',
+                    ],
+                    'mimeTypesMessage' => 'Please upload a valid image file (JPEG, PNG, or GIF).',
+                ]);
+                $errors = $validator->validate($photo, $fileTypeValidator);
+                if (count($errors) > 0) {
+                    $errorMessages = [];
+                    foreach ($errors as $error) {
+                        $errorMessages[] = $error->getMessage();
+                    }
+                    return $this->json($errorMessages, JsonResponse::HTTP_BAD_REQUEST);
+                }
+                $uploadsDirectory = $this->getParameter('uploads_directory');
+                $photoName = md5(uniqid()).'.'.$photo->guessExtension();
+                $photo->move($uploadsDirectory, $photoName);
+                $newmanger->setPicture($photoName);
+                $hashedPassword = $hasher->hashPassword($newmanger, $newmanger->getPassword());
+                $newmanger->setPassword($hashedPassword);
+                $entityManager->persist($newmanger);
+                $entityManager->flush();
+                return $this->json($newmanger, JsonResponse::HTTP_CREATED);
+            }else{
+                return $this->json("Photo must be provided", JsonResponse::HTTP_BAD_REQUEST);
 
-        $hashedPassword = $hasher->hashPassword($manger, $manger->getPassword());
-        $manger->setPassword($hashedPassword);
-
-        $entityManager->persist($manger);
-        $entityManager->flush();
-
-
-        return $this->json($manger, JsonResponse::HTTP_CREATED);
+            }
+       }
     }
     #[Route('/{id}', name: 'app_manger_show', methods: ['GET'])]
-    public function show(PropertyManger $manger): Response
-    {
+    public function show(?PropertyManger $manger): Response
+    {if($manger){
         return $this->json($manger, JsonResponse::HTTP_OK);
+
+    }else return $this->json("Not found", JsonResponse::HTTP_NOT_FOUND);
     }
     #[Route('/{id}/edit', name: 'app_manger_edit', methods: ['Put'])]
     public function edit(
         Request $request,
-        PropertyManger $manger,
+       ? PropertyManger $manger,
         EntityManagerInterface $entityManager,
         SerializerInterface $serializer,
-    ) {
-
+    ) {if ($manger){
         $updatedManger = $serializer->deserialize(
             $request->getContent(),
             PropertyManger::class,
@@ -62,15 +94,20 @@ class PropertyMangerController extends AbstractController
         $entityManager->persist($updatedManger);
         $entityManager->flush();
         return $this->json($updatedManger, JsonResponse::HTTP_CREATED);
+    }else return $this->json("Not found", JsonResponse::HTTP_NOT_FOUND);
+
+       
     }
 
 
 
     #[Route('/delete/{id}', name: 'app_manger_delete', methods: ['DELETE'])]
-    public function delete(Request $request, PropertyManger $manger, EntityManagerInterface $entityManager): JsonResponse
-    {
+    public function delete(? PropertyManger $manger, EntityManagerInterface $entityManager): JsonResponse
+    {if($manger){
         $entityManager->remove($manger);
         $entityManager->flush();
         return new JsonResponse(['message' => 'Manger deleted successfully'], Response::HTTP_OK);
+    }else return $this->json("Not found", JsonResponse::HTTP_NOT_FOUND);
+       
     }
 }
--- src/Controller/ResidentController.php
+++ src/Controller/ResidentController.php
@@ -12,68 +12,70 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer; 
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 #[Route('/Api/resident')]
 class ResidentController extends AbstractController
 {
     #[Route('/', name: 'app_resident')]
     public function getResidents(ResidentRepository $residentRepository): Response
-    {     
-           return $this->json($residentRepository->findAll(),JsonResponse::HTTP_OK);
-    }
-    #[Route('/new', name: 'app_resident_new', methods: [ 'POST'])]
-    public function addResident(Request $request, EntityManagerInterface $entityManager,SerializerInterface $serializer,
-    UrlGeneratorInterface $urlGenerator,UserPasswordHasherInterface $hasher)
     {
-        $resident = $serializer->deserialize($request->getContent(), Resident::class, 'json');
-    
-        $hashedPassword = $hasher->hashPassword($resident, $resident->getPassword());
-        $resident->setPassword($hashedPassword);
-        
-        $entityManager->persist($resident);
-        $entityManager->flush();
-    
-        
-        return $this->json($resident, JsonResponse::HTTP_CREATED);
-   
-        
-
+        return $this->json($residentRepository->findAll(), JsonResponse::HTTP_OK);
+    }
+    #[Route('/new', name: 'app_resident_new', methods: ['POST'])]
+    public function addResident(ResidentRepository $resident, Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, UserPasswordHasherInterface $hasher)
+    {
+        $newresident = $serializer->deserialize($request->getContent(), Resident::class, 'json');
+        $existedemail = $resident->findOneBy(['email' => $newresident->getEmail()]);
+        $existedcin = $resident->findOneBy(['cin' => $newresident->getCin()]);
+        if ($existedemail || $existedcin) {
+            return $this->json("alerdy exist", JsonResponse::HTTP_FORBIDDEN);
+        } else {
+            $hashedPassword = $hasher->hashPassword($newresident, $newresident->getPassword());
+            $newresident->setPassword($hashedPassword);
+            $entityManager->persist($newresident);
+            $entityManager->flush();
+            return $this->json($newresident, JsonResponse::HTTP_CREATED);
+        }
     }
     #[Route('/{id}', name: 'app_resident_show', methods: ['GET'])]
-    public function finResident(Resident $resident): Response
+    public function finResident(?Resident $resident): Response
     {
-        return $this->json($resident,JsonResponse::HTTP_OK);
+        if ($resident){
+            return $this->json($resident, JsonResponse::HTTP_OK);
+
+        }else return $this->json("Not found", JsonResponse::HTTP_NOT_FOUND);
     }
-        #[Route('/{id}/edit', name: 'app_resident_edit', methods: ['Put'])]
-    public function updateResident(Request $request, Resident $resident,
-     EntityManagerInterface $entityManager,
-     SerializerInterface $serializer,
-     )
-    {
-        try {
-            $updatedResident = $serializer->deserialize($request->getContent(), 
-            Resident::class, 
-            'json', 
-            [AbstractNormalizer::OBJECT_TO_POPULATE => $resident]);
-            
-            $entityManager->persist($updatedResident);
-            $entityManager->flush();
-            return $this->json($updatedResident,JsonResponse::HTTP_CREATED);
-        } catch (\Throwable $th) {
-            return $this->json($$th,JsonResponse::HTTP_BAD_REQUEST);
+    #[Route('/{id}/edit', name: 'app_resident_edit', methods: ['Put'])]
+    public function updateResident(
+        Request $request,
+        ?Resident $resident,
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer,
+    ) {if($resident){
+        $updatedResident = $serializer->deserialize(
+            $request->getContent(),
+            Resident::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $resident]
+        );
 
-        }
-      
-
+        $entityManager->persist($updatedResident);
+        $entityManager->flush();
+        return $this->json($updatedResident, JsonResponse::HTTP_CREATED);
+    }else return $this->json("Not found", JsonResponse::HTTP_NOT_FOUND);
+           
+        
     }
     #[Route('/delete/{id}', name: 'app_resident_delete', methods: ['DELETE'])]
-    public function deleteResident(Request $request, Resident $resident, EntityManagerInterface $entityManager): JsonResponse
-    {
-            $entityManager->remove($resident);
-            $entityManager->flush();
-            return new JsonResponse(['message' => 'Resident deleted successfully'], Response::HTTP_OK);
-       
+    public function deleteResident(? Resident $resident, EntityManagerInterface $entityManager): JsonResponse
+    {if ($resident){
+
+        $entityManager->remove($resident);
+        $entityManager->flush();
+        return new JsonResponse(['Resident deleted successfully'], Response::HTTP_OK);
+    }else return $this->json("Not found", JsonResponse::HTTP_NOT_FOUND);
     }
 }
